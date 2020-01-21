00f550974c4f9c1de1b521483edbd381fcb37785
benjamin thom, elliot Jordan and jeremy kale padot <benjamin@>
2020-01-21 11:36:45 -0800

    add brooke edits to remove 4 as new2 result

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
   $("#favorite").submit(function(event) {
     var array1 = [$('#userThing1').val(), $('#userThing2').val(), $('#userThing3').val()];
     //console.log(userThing1, userThing2, userThing3);
-    //console.log(array1)
+    console.log(array1);
+    
+    array1.push(array1[2]);
+    var array2 = array1.slice();
+    console.log(array1);
+    console.log(array2);
 
     $("#output").show();
     $("#userThing").text(array1);
+    $("#new").text(array1[2]);
+    $("#new2").text(array2);
+
     event.preventDefault();
   });
 });
