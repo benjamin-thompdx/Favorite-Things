c618c9ab51b97dc2aad24b2c9ca23f28332f708f
benjamin thom, elliot Jordan and jeremy kale padot <elliot@>
2020-01-21 11:04:59 -0800

    simplify js scripts by com[iling variable conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
   $("#favorite").submit(function(event) {
-    var userThing1 = $('#userThing1').val();
-    var userThing2 = $('#userThing2').val();
-    var userThing3 = $('#userThing3').val();
-    var array1 = [userThing1, userThing2, userThing3];
+    var array1 = [$('#userThing1').val(), $('#userThing2').val(), $('#userThing3').val()];
     //console.log(userThing1, userThing2, userThing3);
     //console.log(array1)
 
